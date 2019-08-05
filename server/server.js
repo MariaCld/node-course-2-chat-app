bf083c7d476b598028319cabe654f46eaf942dcc
Maria Dobre <dma@EBNER.cc>
2019-08-05 17:27:35 +0300

    added css

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const { generateMessage,generateLocationMessage } = require('./utils/message');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -16,16 +16,15 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
-
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
-
     socket.emit('newMessage', {
         from: 'Admin',
         text: 'Welcome to the chat app',
         createdAt: new Date().getTime()
     });
-
+    socket.on('createMessage', (message, callback) => {
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback();
+    })
     socket.on('createLocationMessage', (coords) => {
         io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
     })
