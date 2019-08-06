b91cdbd3dbe4186207b9ba4645775a199f187c68
Maria Dobre <dma@EBNER.cc>
2019-08-06 10:36:20 +0300

    added moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -13,20 +13,23 @@ socket.on('newEmail', function (email) {
 })
 
 socket.on('newMessage', function (message) {
+
     console.log('newMessage', message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`)
+    li.text(`${message.from} ${formattedTime}: ${message.text}`)
 
     jQuery('#messages').append(li);
 })
 
 socket.on('newLocationMessage', function (message) {
-    console.log('newLocationMessage', message);
 
+    console.log('newLocationMessage', message);
+    var formattedLocationTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedLocationTime}: `);
     a.attr('href', message.url);
     li.append(a);
 
