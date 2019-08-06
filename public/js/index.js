731bac38234558ab9445412bac8d3656ef43b666
Maria Dobre <dma@EBNER.cc>
2019-08-06 11:20:48 +0300

    add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -13,27 +13,27 @@ socket.on('newEmail', function (email) {
 })
 
 socket.on('newMessage', function (message) {
-
-    console.log('newMessage', message);
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`)
-
-    jQuery('#messages').append(li);
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 })
 
 socket.on('newLocationMessage', function (message) {
 
-    console.log('newLocationMessage', message);
-    var formattedLocationTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#location-message-template').html();
 
-    li.text(`${message.from} ${formattedLocationTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-
-    jQuery('#messages').append(li);
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 })
 
 
